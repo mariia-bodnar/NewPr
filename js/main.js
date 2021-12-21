25efe82798d4f4432f239742e0ac388d36e26f86
mariia-bodnar <mbodnar548@gmail.com>
2021-12-21 19:19:16 +0200

    added media-querry and close button for left menu

--- js/main.js
+++ js/main.js
@@ -77,24 +77,31 @@ $(document).ready(() => {
       $('.line_3').css('width', '15px').css('transition', '0.5s')
     })
     $('.header__more-menu').mouseleave(() => {
-      $('.line_1').css('width', '15px').css('transition', '0.5s')
-      $('.line_3').css('width', '25px').css('transition', '0.5s')
+      $('.line_1').css('width', '15px').css('transition', '0.5s');
+      $('.line_3').css('width', '25px').css('transition', '0.5s');
     })
     $('.header__more-menu').click(() => {
-      $('.line_1').toggleClass('display_none')
-      $('.line_2').toggleClass('transition_2')
-      $('.line_3').toggleClass('display_none')
-      $('#line_4').toggleClass('line_4')
-      $('.lines').toggleClass('gap')
-      $('#left_menu').toggleClass('wrapper_popular')
-      $('.container').toggleClass('conteiner_moove')
+      $('.close_tag_button').toggleClass("display_block");
+      $('.lines').toggleClass('display_none ');
+      $('#left_menu').toggleClass('size_menu');
+      // $('.size_menu').css("height","100%")
+      // $('.container').toggleClass('conteiner_moove');
+    })
+    // $(".close_tag_button").click (()=>{
+      
+    // })
+    $(".close_tag").click (()=>{
+      // $('.conteiner').toggle();
+      $(".size_menu").toggle();
+      $('.close_tag_button').toggleClass("display_none_4");
+      // $('header__more-menu.display_none').toggle();
     })
     $('.header__search').click(() => {
-      $('.header__search').css('display', 'none')
-      $('.header__social-wrap').toggleClass('display_none')
-      $('.input').toggleClass('input__hide')
+      $('.header__search').css('display', 'none');
+      $('.header__social-wrap').toggleClass('display_none');
+      $('.input').toggleClass('input__hide');
     })
-  
+
     // $('body').not(' .header__search').click(() => {
     //   $(".input__hide").css("display","none");
     //   $(".header__search").show();
@@ -111,6 +118,40 @@ $(document).ready(() => {
       infinite: true,
       slidesToShow: 4,
       slidesToScroll: 2,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 2,
+          },
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            infinite: true,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          },
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 420,
+          settings: {
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
     })
   })
   $(document).ready(function () {
@@ -163,33 +204,33 @@ $(document).ready(() => {
   //     $('#entertaiment_menu').stop().slideToggle().css('display', 'block')
   //   });
 
-
   // }
   // $(function () {
   //   MenuQuery()
   // })
-  $('.menu_button').on('click',  ()=>{
-    $('#menu_media').toggle();
+  $('.menu_button').on('click', () => {
+    $('#menu_media').toggle()
   });
-  
-  $('#layout').on('click',  ()=>{
-    $('#layout_menu').toggle();
+
+  $('#layout').on('click', () => {
+    $('#layout_menu').toggle()
   });
-  $('#home').on('click',  ()=>{
-    $('#home_menu').toggle();
+  $('#home').on('click', () => {
+    $('#home_menu').toggle()
   });
-  
-  $('#category').on('click',  ()=>{
-    $('#category_menu').toggle();
+
+  $('#category').on('click', () => {
+    $('#category_menu').toggle()
   });
-  $('#posts').on('click',  ()=>{
-    $('#posts_menu').toggle();
+  $('#posts').on('click', () => {
+    $('#posts_menu').toggle()
   });
-  
-  $('#pages').on('click',  ()=>{
+
+  $('#pages').on('click', () => {
     $('#pages_menu').toggle();
   });
-  $('#entertaiment').on('click',  ()=>{
-    $('#entertaiment_menu').toggle();
-  });  
-});
+  $('#entertaiment').on('click', () => {
+    $('#entertaiment_menu').toggle()
+  });
+})
+;
